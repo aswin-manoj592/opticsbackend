793affd351de9dd4dfffe0dc3a4155c42b2c926a
aswin-manoj592 <aswinkannur592@gmail.com>
2026-05-20 10:08:17 +0530

    hoome product added

--- src/admin/branch/branch.module.ts
+++ src/admin/branch/branch.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AdminBranch } from './branch.entity';
+import { AdminUser } from '../user/user.entity';
 import { BranchController } from './branch.controller';
 import { BranchService } from './branch.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AdminBranch])],
+  imports: [TypeOrmModule.forFeature([AdminBranch, AdminUser])],
   controllers: [BranchController],
   providers: [BranchService],
   exports: [BranchService]
--- src/admin/branch/branch.service.ts
+++ src/admin/branch/branch.service.ts
@@ -2,12 +2,35 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { AdminBranch } from './branch.entity';
+import { AdminUser } from '../user/user.entity';
 
 @Injectable()
 export class BranchService {
-  constructor(@InjectRepository(AdminBranch) private repo: Repository<AdminBranch>) {}
+  constructor(
+    @InjectRepository(AdminBranch) private repo: Repository<AdminBranch>,
+    @InjectRepository(AdminUser) private userRepo: Repository<AdminUser>
+  ) {}
 
-  create(data: any) { return this.repo.save(data); }
-  findAll() { return this.repo.find(); }
+  async create(data: any) { 
+    const branch = this.repo.create({ name: data.name, city: data.city });
+    const savedBranch = await this.repo.save(branch);
+
+    if (data.managerName && data.managerEmail && data.managerPassword) {
+      const user = this.userRepo.create({
+        name: data.managerName,
+        email: data.managerEmail,
+        phone: data.managerPhone || '',
+        password: data.managerPassword,
+        role: 'manager',
+        branch: savedBranch
+      });
+      await this.userRepo.save(user);
+    }
+    
+    return savedBranch;
+  }
+  
+  findAll() { return this.repo.find({ relations: ['users'] }); }
+  
   remove(id: number) { return this.repo.delete(id); }
 }
